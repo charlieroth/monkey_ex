72229a8aa1c89b4cf65b0024e32e79ee04f841d6
charlieroth <charlieroth4@icloud.com>
2023-05-22 15:19:46 +0200

    repl: initial implementation

--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,21 @@
 const std = @import("std");
+const repl = @import("repl.zig");
 
+const mem = std.mem;
+const heap = std.heap;
+const fs = std.fs;
+const io = std.io;
 const debug = std.debug;
 
 pub fn main() !void {
-    debug.print("mirlang\n", .{});
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    var allocator: mem.Allocator = gpa.allocator();
+    defer {
+        const deinit_check = gpa.deinit();
+        debug.assert(deinit_check == .ok);
+    }
+
+    const stdin: fs.File.Reader = io.getStdIn().reader();
+    const stdout: fs.File.Writer = io.getStdOut().writer();
+    try repl.start(allocator, stdin, stdout);
 }
---
+++ src/repl.zig
@@ -0,0 +1,30 @@
+const std = @import("std");
+const Lexer = @import("lexer.zig").Lexer;
+const token = @import("token.zig");
+
+const fs = std.fs;
+const mem = std.mem;
+const testing = std.testing;
+const debug = std.debug;
+
+const PROMPT = ">> ";
+
+pub fn start(allocator: mem.Allocator, stdin: fs.File.Reader, stdout: fs.File.Writer) !void {
+    while (true) {
+        _ = try stdout.write(PROMPT);
+        const input = try stdin.readUntilDelimiterAlloc(allocator, '\n', 1000);
+        var lexer = Lexer.init(allocator, input);
+
+        // TODO(charlieroth): Might be a MacOS/iTerm thing but there seems to
+        // always be an "illegal" character, (170) at the start of the input
+        // string so consume an extra token to avoid this. Look into this
+        // if it causes any issues. Might need to run repl with "raw" mode.
+        // After implementation is complete might be good to integrate
+        // https://github.com/joachimschmidt557/linenoize into the project/repl.
+        var tok = lexer.next_token();
+        tok = lexer.next_token();
+        while (tok.t != token.TokenType.Eof) : (tok = lexer.next_token()) {
+            debug.print("[{any}]::[{s}]\n", .{ tok.t, tok.literal });
+        }
+    }
+}

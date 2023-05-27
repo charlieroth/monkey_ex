11180823ee4695c0f202533b9759794fe464b43a
charlieroth <charlieroth4@icloud.com>
2023-05-27 12:00:31 +0200

    forgot to rename mix project

--- mix.exs
+++ mix.exs
@@ -1,9 +1,9 @@
-defmodule Mirlang.MixProject do
+defmodule Monkeyex.MixProject do
   use Mix.Project
 
   def project do
     [
-      app: :mirlang,
+      app: :monkeyex,
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
@@ -11,19 +11,14 @@ defmodule Mirlang.MixProject do
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger],
-      mod: {Mirlang.Application, []}
+      mod: {Monkeyex.Application, []}
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
-    [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-    ]
+    []
   end
 end

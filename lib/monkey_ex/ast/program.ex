7e56dc563c82d32afb14ec820cbdb2abd84bd4f7
charlieroth <charlieroth4@icloud.com>
2023-05-30 11:26:24 +0200

    program: add token_literal function

--- lib/monkey_ex/ast/program.ex
+++ lib/monkey_ex/ast/program.ex
@@ -6,11 +6,17 @@ defmodule MonkeyEx.Ast.Program do
   @enforce_keys [:statements]
   defstruct [:statements]
 
-  @type t :: %{
-          statements: list(any())
-        }
-
   alias MonkeyEx.Ast.Node
+
+  def token_literal(program) do
+    if not Enum.empty?(program.statements) do
+      program.statements
+      |> List.first()
+      |> Node.token_literal()
+    else
+      ""
+    end
+  end
 
   def string(program) do
     program.statements
